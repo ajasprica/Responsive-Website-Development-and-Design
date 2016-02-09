9d793a3b67c5b1ff5a053c31d9881828f596572c
Anto Jasprica <anto.jasprica@gmail.com>
2016-02-09 08:24:06 +0100

    rename folders

--- 1.9_Final-Project/buttonCollapse.js
+++
@@ -1,7 +0,0 @@
- $(document).ready(function() {
-    $("#but1").click(function(){
-        $("#light").slideToggle("slow", function(){ 
-            $("#but1").html($(this).is(":hidden") ? "Expand" : "Collapse");  
-        });
-    });
-    });
